cfa91eee4635bdfd072f56a1a9b903e1abf82d4c
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-07-23 16:58:47 -0300

    struct to test read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{env, fs::File, io::prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+    //if we need to use non unicode characters its necessary to use args_os instead that uses 
+    //OsStrings rather than Strings. Otherwise OsStrings values differ to plattaform
+    
+    let query = &args[1];
+    let filename = &args[2];
+    
+    println!("Searching for {}", query);
+    
+    println!("In file {}", filename);
+
+    let mut f = File::open("").expect("File not found");
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+    println!("With text:\n{}", contents);
+
 }
