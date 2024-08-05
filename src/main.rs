ca5d4b31b9d6863f5c0d487c2ade976c43f26258
Victor Giuliano <vitor.giuliano@gmail.com>
2024-08-04 23:08:45 -0300

    update to exempla of 2nd edition of the book

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::{env, fs::File, io::prelude::*};
+use std::{env, fs::{self, File}, io::prelude::*};
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
+    /* let args: Vec<String> = env::args().collect();
     //if we need to use non unicode characters its necessary to use args_os instead that uses 
     //OsStrings rather than Strings. Otherwise OsStrings values differ to plattaform
     
@@ -17,6 +17,28 @@ fn main() {
     let mut contents = String::new();
 
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
+    println!("With text:\n{}", contents); */
+
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("With text:\n{}", contents);
 
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
+}
+
+struct Config {
+    query: String,
+    file_path: String,
 }
