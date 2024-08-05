f3f596ff806f335d61c60ab083ca050563e2d58b
Victor Giuliano <vitor.giuliano@gmail.com>
2024-08-05 00:00:21 -0300

    error handling improvment

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs::{self, File}, io::prelude::*};
+use std::{env, fs::{self, File}, io::prelude::*, process};
 
 fn main() {
 
@@ -21,7 +21,11 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    //let config = Config::build(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
