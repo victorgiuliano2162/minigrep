291bbe749e5736676213bd56b1a1bfbd5b4c7dbd
Victor Giuliano <vitor.giuliano@gmail.com>
2024-08-12 12:56:06 -0300

    implement method for seacr queries

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
+use std::env;
 use std::{error::Error, fs};
-
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -11,15 +12,28 @@ impl Config {
         if args.len() < 3 {
             return Err("NOT ENOUGH ARGUMENTS");
         }
-        let query =args[1].clone();
+
+        let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
 
     //println!("With text:\n{contents}");
     for line in search(&config.query, &contents) {
@@ -29,31 +43,26 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
-}
-
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut v = Vec::new();
 
     for line in contents.lines() {
         if line.contains(query) {
             v.push(line.trim())
-        } 
+        }
     }
     v
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_ascii_lowercase();
+    let query = query.to_lowercase();
     let mut v = Vec::new();
 
     for line in contents.lines() {
-        if line.to_ascii_lowercase().contains(&query) {
+        if line.to_lowercase().contains(&query) {
+            println!(" a{line}a ");
             v.push(line.trim())
+            //for some reason, just two itens pushed to 
         }
     }
     v
@@ -85,7 +94,7 @@ mod tests {
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
- 
+
     #[test]
     fn case_insensitive() {
         let query = "rUsT";
@@ -95,6 +104,9 @@ mod tests {
         Pick three.
         Trust me.";
 
-        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
